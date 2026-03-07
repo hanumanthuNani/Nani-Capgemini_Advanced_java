730f16c4f3c4ad39e56b91274e7d58cb6a9940cc
hanumanthuNani <nanihanumanthu8@gmail.com>
2026-03-08 01:50:50 +0530

    RestaurantApi added

---
+++ Assesments/Assesment_4/restaurantapi/src/main/java/com/restaurantapi/RestaurantapiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantapiApplication.class, args);
+	}
+
+}
---
+++ Assesments/Assesment_4/restaurantapi/src/main/java/com/restaurantapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.restaurantapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
